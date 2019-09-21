6d0d690c702d09f4852d08b752a9721875659e91
Diego Alves <diego.alves0688@gmail.com>
2019-09-21 19:16:34 -0300

    first functional chart

--- app/src/main/java/com/example/financial_manager/MainActivity.java
+++ app/src/main/java/com/example/financial_manager/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.financial_manager;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -27,6 +28,15 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.List;
+
+import lecho.lib.hellocharts.model.Axis;
+import lecho.lib.hellocharts.model.AxisValue;
+import lecho.lib.hellocharts.model.Line;
+import lecho.lib.hellocharts.model.LineChartData;
+import lecho.lib.hellocharts.model.PointValue;
+import lecho.lib.hellocharts.model.Viewport;
+import lecho.lib.hellocharts.view.LineChartView;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -91,14 +101,74 @@ public class MainActivity extends AppCompatActivity
             @Override
             public void onClick(View view) {
 
-                dbmanager.clearData();
-                dataList.clear();
-                adapter.notifyDataSetChanged();
+                //dbmanager.clearData();
+                //dataList.clear();
+                //adapter.notifyDataSetChanged();
+
+                initiateChart("new data");
 
                 ShowMessage("All data deleted.");
 
             }
         });
+
+        initiateChart("start");
+
+
+    }
+
+    // Chart lib reference: https://github.com/lecho/hellocharts-android
+    //
+    // Thank you to Sveta post in Mobindustry: 
+    // https://www.mobindustry.net/how-to-quickly-implement-beautiful-charts-in-your-android-app/
+
+    public void initiateChart(String title){
+
+        LineChartView lineChartView;
+        String[] axisData = {"Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sept",
+                "Oct", "Nov", "Dec"};
+        int[] yAxisData = {50, 20, 15, 30, 20, 60, 15, 40, 45, 10, 90, 18};
+
+        lineChartView = findViewById(R.id.chart);
+
+        List yAxisValues = new ArrayList();
+        List axisValues = new ArrayList();
+
+
+        Line line = new Line(yAxisValues).setColor(Color.parseColor("#9C27B0"));
+
+        for (int i = 0; i < axisData.length; i++) {
+            axisValues.add(i, new AxisValue(i).setLabel(axisData[i]));
+        }
+
+        for (int i = 0; i < yAxisData.length; i++) {
+            yAxisValues.add(new PointValue(i, yAxisData[i]));
+        }
+
+        List lines = new ArrayList();
+        lines.add(line);
+
+        LineChartData data = new LineChartData();
+        data.setLines(lines);
+
+        Axis axis = new Axis();
+        axis.setValues(axisValues);
+        axis.setTextSize(16);
+        axis.setTextColor(Color.parseColor("#03A9F4"));
+        data.setAxisXBottom(axis);
+
+        Axis yAxis = new Axis();
+        yAxis.setName(title);
+        yAxis.setTextColor(Color.parseColor("#03A9F4"));
+        yAxis.setTextSize(16);
+        data.setAxisYLeft(yAxis);
+
+        lineChartView.setLineChartData(data);
+        Viewport viewport = new Viewport(lineChartView.getMaximumViewport());
+        viewport.top = 110;
+        lineChartView.setMaximumViewport(viewport);
+        lineChartView.setCurrentViewport(viewport);
+
     }
 
     @Override
